17247d8710b0c161d663cfd8ba5e08230d00b0c1
Michal Konczak <michal.stefan.konczak@gmail.com>
2020-12-29 13:34:08 +0100

    updating gitignore and removing yarn.lock

--- dist/cats/cats.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { CatsService, Cat } from './cats.service';
-export declare class CatsController {
-    private catsService;
-    constructor(catsService: CatsService);
-    getAll(): Cat[];
-    create(body: {
-        name: string;
-    }): Cat;
-}
