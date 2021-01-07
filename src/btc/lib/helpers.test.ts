e15c48ba046f842e54f3ed178d334569d81dadd6
Michal Konczak <michal.stefan.konczak@gmail.com>
2021-01-07 19:04:21 +0100

    deploy-commit

--- src/btc/lib/helpers.test.ts
+++ src/btc/lib/helpers.test.ts
@@ -19,10 +19,19 @@ const bids: BibtayResources['bids'] = [
   [29501.1, 0.00376257],
   [29300.02, 0.08859038],
   [29001.1, 0.00382744],
+  [28501.01, 0.0038946],
+  [28500.04, 0.02191085],
+  [28500.02, 0.09107713],
+  [28500, 0.06399333],
+  [28340.0, 0.01752752],
+  [28260, 0.10531812],
+  [28001.1, 0.00396413],
+  [28000, 0.00017893],
+  [27999.99, 0.05420288],
 ];
 
 describe('getOrderBookValues', () => {
-  it('returns values for 1BTC', () => {
+  it(`checks USD value for ${bids[0][1]}BTC`, () => {
     const [bidsSample] = bids;
     const [pricePerOne, btcAmount] = bidsSample;
 
@@ -33,5 +42,13 @@ describe('getOrderBookValues', () => {
 
     expect(btcSum).toEqual(btcAmount);
     expect(usdValue).toEqual(pricePerOne * btcAmount);
+  });
+  it('checks USD value for 1BTC', () => {
+    const [usdValue, btcSum] = getOrderBookValues({
+      btcAmount: 1,
+      orderBookValueList: bids,
+    });
+
+    expect(btcSum).toEqual(1);
   });
 });
